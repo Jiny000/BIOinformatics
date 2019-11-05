dda982b792c55bf8474d6d0a9fa680eade206caf
Jiny000 <821357094@qq.com>
2019-11-05 22:36:55 +0800

    Update requests.py

--- python/requests.py
+++ python/requests.py
@@ -6,10 +6,24 @@ with open('F:/ljy/atom/markerf.txt',encoding='utf-8') as m:
     with open('F:/ljy/atom/primer.txt','w',encoding='utf-8') as f:
         for i in marker_line:
             key=i
-            #try:
-            r =requests.get(f"https://wheat.pw.usda.gov/cgi-bin/GG3/report.cgi?class=marker;query=*{key}*;name={key}")
-            t = r.text
-            primer=re.findall(compiles,t)
-            print(f'{key}',primer)
-            f.write(f'{key}')
-            f.write(f'{primer}')
+            headers ={'user-agent':"Chrome"}
+            url=f"https://wheat.pw.usda.gov/cgi-bin/GG3/report.cgi?class=marker;query=*{key}*;name={key}"
+            try:
+                if count<3:
+                count=count+1
+                get_marker(url,key,headers)
+            except :
+                print(f"{key} is Invalid connect")
+                f.write(f"{key} is lost.")
+                f.write('\n')
+
+def get_marker(url,key,headers):
+    r =requests.get(url,headers=headers,timeout=10)
+    print(r.status_code)
+    #if  r.status_code == 200：
+    t = r.text
+    primer=re.findall(compiles,t)
+    print(f'{key}',primer)
+    f.write(f'{key}')
+    f.write(f'{primer}')
+    f.write('\n')
